21183009aef1d6c615501c37586e1db7220de2d3
User <shevkar.sanket@gmail.com>
2021-05-02 18:39:22 +0530

    chore(client): Redux setup done
    Signed-off-by: User <shevkar.sanket@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,37 @@
 import './App.css';
+import { connect } from "react-redux"
 
-function App() {
+import {
+  increaseCounter,
+  decreaseCounter,
+} from "./redux/Counter/counter.actions"
+
+
+function App(props) {
   return (
     <div className="App">
-      App
+      <div>Count: {props.count}</div>
+
+      <button onClick={() => props.increaseCounter()}>Increase Count</button>
+
+      <button onClick={() => props.decreaseCounter()}>Decrease Count</button>
     </div>
   );
 }
 
-export default App;
+
+const mapStateToProps = state => {
+  return {
+    count: state.counter.count,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    increaseCounter: () => dispatch(increaseCounter()),
+
+    decreaseCounter: () => dispatch(decreaseCounter()),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
